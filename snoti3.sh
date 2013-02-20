1f292dc4335f77e46b5a87635ebc056f3b0a5a7a
Lukas Senger <lukas@fridolin.com>
2013-02-20 17:09:52 +0100

    cleaner snoti3.sh

--- snoti3.sh
+++ snoti3.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 # bash script to prepend i3status with the output of a background process
 
-# basic configuration:
-format="%a: [%s] %b >%q"
-color="#00FFFF"
-bin="snot -f"
+bin="snot"
+color="#87afff"
 
 
 
-exec 3< <($bin "$format")
-
-echo "{\"version\":1}" || exit 1
-echo "[" || exit 1
+exec 3< <("$bin")
 
 run=0
 i3status | while :
 do
     read i3line
+    # let i3status handle the first two lines:
+    #   {\"version\":1}
+    #   [
     if [ $run -lt 2 ]
     then
+        echo "$i3line"
         let run++
         continue
     fi
     read -t 1 bgline <&3
+    # first data line without preceding comma
     if [ $run -eq 2 ]
     then
         let run++
-        echo "[{\"full_text\":\"$bgline<--\"},${i3line:1}" || exit 1
+        echo "[{\"full_text\":\"$bgline\",\"color\":\"$color\"},${i3line:1}" || exit 1
         continue
     fi
-    echo ",[{\"full_text\":\"$bgline<--\",\"color\":\"$color\"},${i3line:2}" || exit 1
+    # normal operation
+    echo ",[{\"full_text\":\"$bgline\",\"color\":\"$color\"},${i3line:2}" || exit 1
 done 
-
